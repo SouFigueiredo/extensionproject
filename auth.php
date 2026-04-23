bba787757d307e7ca8775693e83e94b1529e6adf
Gabriel Figueiredo <gabriel.contato.oficial@gmail.com>
2026-04-23 20:25:20 -0300

    Arquivos Iniciais

---
+++ auth.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+include("connect.php");
+
+$user = $_POST['inUser'];
+$pass = $_POST['inPass'];
+
+$user = $conn->real_escape_string($user);
+
+$sql = "SELECT * FROM usuarios WHERE usuario = '$user'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $dados = $result->fetch_assoc();
+
+    if (password_verify($pass, $dados['senha'])) {
+        $_SESSION['usuario'] = $dados['usuario'];
+        echo "Login realizado com sucesso!";
+    } else {
+        echo "Senha incorreta!"
+    }
+} else  {
+    echo "Usuário não encontrado!"
+}
---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'projetoext'
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if($conn->connect_error) {
+    die("Erro na conexao: " . $conn->connect_error);
+}
+?>
